8a3cfac69e050197b7600d79ab9a283ec58e65ee
AgustinCelis <agus.kod@gmail.com>
2022-04-27 18:11:35 -0300

    Antes de usar template engines

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath));
+
+app.listen(4000, () =>{
+    console.log('Server on port 4000')
+})
+
+app.get('/', (req, res) =>{
+    res.status(200).sendFile(path.join(__dirname, 'views/home.html'))
+})
+
+app.get('/detalle', (req, res) =>{
+    res.status(200).sendFile(path.join(__dirname, 'views/detail.html'))
+})
