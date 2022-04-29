4f7e079f034dbfd5d4e0a23d9ffe8d51797193b6
AgustinCelis <agus.kod@gmail.com>
2022-04-29 20:09:38 -0300

    Entrega sprint

--- src/app.js
+++ src/app.js
@@ -6,14 +6,29 @@ const app = express();
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.listen(4000, () =>{
     console.log('Server on port 4000')
 })
 
 app.get('/', (req, res) =>{
-    res.status(200).sendFile(path.join(__dirname, 'views/home.html'))
+    res.render('home.ejs');
 })
 
 app.get('/detalle', (req, res) =>{
-    res.status(200).sendFile(path.join(__dirname, 'views/detail.html'))
+    res.render('detail.ejs')
+})
+
+app.get('/carrito', (req, res) =>{
+    res.render('cart.ejs')
+})
+
+app.get('/login', (req, res) =>{
+    res.status(200).sendFile(path.join(__dirname, 'views/login.html'))
+})
+
+app.get('/register', (req, res) =>{
+    res.status(200).sendFile(path.join(__dirname, 'views/register.html'))
 })
